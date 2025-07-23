aa20f61bcb605eb5901e50806e1cbe18cb9eb0b7
venkat srinivas <cvenkatasrinivasula.reddy@gmail.com>
2025-07-23 05:31:42 +0000

    code  deploy adding models

---
+++ models/emp_2.sql
@@ -0,0 +1,16 @@
+
+select 1 id,'shanthi' name
+union all
+select 2 id,'suja' name
+union all
+select 3 id,'ammu' name
+union all
+select 4 id,'prabha' name
+union all
+select 5 id,'kalpi' name
+union all
+select 6 id,'krishnaveni' name
+union all
+select 7 id,'veneela' name
+union all
+select 8 id,'chinni' name
